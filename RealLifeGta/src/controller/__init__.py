3633c2c56f9a3e66484f388a0ecc8fbc6681d657
Nakul Dhande <nakul225@gmail.com>
2018-01-14 11:20:50 -0800

    1) Removing .pyc files 2) Added gitignore entry for .pyc 3) Added changes to create data directory structure if it doesn't exist

--- RealLifeGta/src/controller/__init__.py
+++ RealLifeGta/src/controller/__init__.py
@@ -2,18 +2,26 @@ import traceback
 import sys
 import pickle
 import os
+import subprocess
 from src.models.Life import Life
 from src.view.CommandLineInterface import CommandLineInterface
 
 def main():
     #initialize
-    life_filename = "/home/nakul/Documents/RealLifeGTA/gta.data"
+    # Assumes Linux setup
+    cmd="whoami 2>/dev/null >/tmp/username"
+    username=subprocess.check_output("whoami",shell=True).split()
+
+    life_directory = "/home/"+username[0]+"/Documents/RealLifeGTA/"
+    life_filename="gta.data"
+    life_complete_path=life_directory+life_filename
     try:
-        life = _load_from_file(life_filename)
-        print "Loading life from datastore"
+        life = _load_from_file(life_complete_path)
+        #print "Loading life from datastore"
     except:
-        traceback.print_exc(file=sys.stdout)
-        print "Couldn't load life from datastore, creating a new life"
+        #traceback.print_exc(file=sys.stdout)
+        print "Couldn't load life from datastore, creating a new file at path:" +life_directory
+        subprocess.check_output("mkdir -p "+life_directory,shell=True)
         life = Life()
 
     # start main loop
@@ -21,7 +29,7 @@ def main():
 
     #persist data
     try:
-        _save_to_file(life, life_filename)
+        _save_to_file(life, life_complete_path)
     except:
         traceback.print_exc(file=sys.stdout)
 
